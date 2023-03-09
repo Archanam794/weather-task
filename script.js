69813ba4ec3ed265706e03ab9a957681a9351365
Archana M <122900221+Archanam794@users.noreply.github.com>
2023-03-09 16:43:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,146 +1,88 @@
-let cardData=[];
-let datav=[];
-let chapsData=[];
-///////////////////////////////////////
-//////////fetching url using async/////
-//////////////////////////////////////
- async function fetchdata(){
-
-    var url=`https://anapioficeandfire.com/api/books?pageSize=50`;
-    try{
-        let result=await fetch(url);
-        let output=await result.json();
-        //console.log(output);
-        if(output.length>0){
-        datav=[...output];}
-       createCard(datav);
-      // createchaps(datav);
-       
-        
-    }
-    catch(error){
-        console.log(error);
-    }
- }
- fetchdata();
-
- 
- const contain=document.createElement('div');
-contain.setAttribute('class','container');
-
-///////////////////////////////////////////
-///////////// search box //////////////////
-//////////////////////////////////////////
-
-let input=document.createElement('input');
-input.setAttribute("id","bkname");
-input.placeholder="Enter Book ";
-input.type="text";
-input.onkeyup=function(){
-handleSearch(this);}
-
-
-document.body.append(input,contain);
-
-//////////////////////////////////////////////////
-// create function a div to display book details//
-//////////////////////////////////////////////////
-
-function createCard(value){
-    const carder=document.createElement('div');
-    carder.setAttribute('class','cardee');
-    contain.innerHTML='';
-  // console.log(value);
-    value.forEach((data)=>{
-    
-    const card = document.createElement('div');
-    card.classList.add('cards');
-
-    let Name=document.createElement('h4');
-    Name.textContent=`${data.name}`;
-    Name.setAttribute('class','name');
-    let Isbn=document.createElement('p');
-    Isbn.setAttribute('class','isbn');
-    Isbn.innerText=`Isbn:${data.isbn}`;
-    let Numofpages=document.createElement('p');
-    Numofpages.setAttribute('class','nopge');
-    Numofpages.innerText=`Number of the Pages:${data.numberofPages}`;
-    let Author=document.createElement('h5');
-    Author.setAttribute('class','author');
-    Author.innerText=`Author:${data.authors}`;
-    let Publisher=document.createElement('p');
-    Publisher.setAttribute('class','publisher');
-    Publisher.innerText=`Publisher:${data.publisher}`;
-    let Release=document.createElement('p');
-    Release.setAttribute('class','releasedate');
-    Release.innerText=`Release Date:${data.released}`;
-    let button=document.createElement('button');
-       
-        button.setAttribute("type","button");
-    button.setAttribute("class","btn btn-secondary");
-    button.innerText="click here characters";
-    button.onclick=function(){
-    details( data.characters);
-  showPopup();}
-       
+let fet = fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data=[]) => { console.log(data);
+    data.map((value)=>{
+        console.log(` ${value.name.common} ${value.flags.png} Capital ${value.capital} Region ${value.region}
+         CountryCode ${value.car.signs} latlng ${value.latlng}`   )
+         
+     const row=document.getElementById('row');
+     const cardiv=document.createElement('div');
+     cardiv.setAttribute('class','row col-lg-4 col-sm-12');
+     // create a card//
+     const card=document.createElement('div');
+     card.setAttribute('class','card');
+     // create card header//
+     const cardheader=document.createElement('h3');
+     cardheader.setAttribute('class','card-header');
+     cardheader.append(value.name.common);
+     card.appendChild(cardheader);
+     cardiv.appendChild(card);
+     row.appendChild(cardiv)
+     //create card body//
+    const cardbody=document.createElement('div');
+     cardbody.setAttribute('class','card-body');
+     // create image div//
+     const imge=document.createElement('div');
+     imge.setAttribute('class','card-img');
+     const flgs=document.createElement('img')
+     flgs.src=value.flags.png;
+     imge.appendChild(flgs);
+     cardbody.appendChild(imge);
+     const br=document.createElement('div');
+     br.setAttribute('class','Text');
+         
+     //create body text//
+     const p1=document.createElement('p');
+     const txt1=document.createTextNode('Capital:');
+     const spc=document.createTextNode(' ');
+     p1.append(txt1,spc,value.capital);
+     br.append(p1);
+     cardbody.appendChild(br);
+     card.appendChild(cardbody);
+     const r1=document.createElement('p');
+     const txt2=document.createTextNode('Region:');
+     const spc1=document.createTextNode(' ');
+     r1.append(txt2,spc1,value.region);
+     cardbody.appendChild(r1);
+     card.appendChild(cardbody);
+     const c1=document.createElement('p');
+     const txt3=document.createTextNode('CountryCode:');
+     const spc2=document.createTextNode(' ');
+     c1.append(txt3,spc2,value.car.signs);
+     cardbody.appendChild(c1);
+     card.appendChild(cardbody);
+     //create a button
+     const button=document.createElement('button');
+     button.setAttribute('class','btn btn-primary');
+     button.textContent='click for weather';
+     button.onclick=function(){
+     getweather(value.latlng);
+     showpop();
+     }
     
 
-   card.append(Name,Isbn,Numofpages,Author,Publisher,Release,button);
-
-carder.appendChild(card);
-contain.appendChild(carder);
-});
-
- }
-//////////////////////////////////////////
-////////search book///////////////////////
-/////////////////////////////////////////
- function handleSearch(target) {
-    const search = target.value.toLowerCase();
-    console.log(datav);
-    const searchMatch = datav.filter((element) => {
-     
-      return  element.name.toLowerCase().includes(search);
-    });
-    createCard(searchMatch);
-  }
-  /////////////////////////////////////////////////
-  /////// create a new popup window///////////////
-  ////////////////////////////////////////////////
-  let newDIV=document.createElement('div');
-    newDIV.setAttribute('class','charc');
-    newDIV.setAttribute('id','bkname1');
-   newDIV.innerHTML='';
-    document.body.append(newDIV);
-    ///////////////////////////////////
-    ////////// get data for button/////
-    ///////////////////////////////////
- async function details(data){
-  
-    
-    for(let i=0;i<5;i++){
-      try{
-        const res=await fetch(data[i]);
-        const res1=await res.json();
-        chapsData.push(res1.name); 
-        console.log(chapsData); newDIV.innerHTML=`<h2>Characters</h2>
-        <p id="p1"> ${chapsData}</p>
-        <button onclick="hidePopup()">Close</button>`
-       
-
+     cardbody.appendChild(button);
+     card.appendChild(cardbody);
+     var pdv = document.createElement("div");
+     pdv.setAttribute("id","popup")
+     pdv.innerHTML=`<h2>Weather Report</h2>
+     <p id="p1"></p>
+     <button onclick="hidePopup()">Close</button>`
+    document.body.append(pdv)
+    })
+    })
+    function hidePopup() {
+      var popup = document.getElementById("popup");
+      if (popup) {
+        popup.style.display = "none";
       }
-    
-      catch(err){
-        alert(err)
+      else {
+        console.error("Popup element not found");
       }
     }
-
-  }
-  /////////////////////////////////////////////////////////////
-  /////////////////popup function/////////////////////////////
-  ////////////////////////////////////////////////////////////
-  function showPopup() {
-    var popup = document.getElementById("bkname1");
+    
+  function showpop() {
+    var popup = document.getElementById("popup");
     if (popup) {
       popup.style.display = "block";
     }
@@ -148,20 +90,47 @@ contain.appendChild(carder);
       console.error("Popup element not found");
     }
   }
-  function hidePopup() {
-    var popup = document.getElementById("bkname1");
-    if (popup) {
-      popup.style.display = "none";
-    }
-    else {
-      console.error("Popup element not found");
-    }
-  }
+     function getweather(value){
+      let lat=value[0];
+      let lng=value[1];
+      weatherupdate(lat,lng);
+     }
+    function weatherupdate (a,b){
+
+      fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${a}&lon=${b}&appid=75e5070a460ff9cce78da8499c2a3eee`)
+         .then((response) => response.json())
+        .then((data)=> {
+          console.log(data);
+          const output=document.getElementById('p1')
+      
+             output.innerText=`
+                     
+             Current Humidity is ${data.main.humidity}
+             Current Pressure is ${data.main.pressure}
+            Current Temperature is ${data.main.temp}`
+            })
+     }
+
+     
+
+
+//  function block(lat,lng,name){
  
-  
 
+   
  
+//    console.log(name)
+//  fetch('https://api.openweathermap.org/data/3.0/onecall?lat='${lat}'&lon='${lon}'&exclude={part}&appid='${75e5070a460ff9cce78da8499c2a3eee}'')
+//   .then((response) => response.json())
+//  .then((data)=> {
 
-
+//       alert(`
+//                 For ${name.id}  
+//       Current Humidity is ${data.main.humidity}
+//       Current Pressure is ${data.main.pressure}
+//      Current Temperature is ${data.main.temp}`)
+//      })
+//  }
   
+//  document.addEventListener("click",(event) => event.preventDefault())
 
